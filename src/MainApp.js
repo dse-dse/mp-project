23f6b9a29c0f49b495f90c4eb266c48075c5f0c7
dse-dse <Manatin228306@Mail.ru>
2025-12-12 20:47:20 +0500

    test

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePageApp from "./homePage/App";
 import RealEstatePageApp from "./realEstatePage/App";
 import "./MainApp.css";
